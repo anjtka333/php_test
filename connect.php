88d0bb7241b3d372d3399b7c3479a7aff6a37911
Anna <andreikiv.ann@gmail.com>
2017-03-19 17:38:33 +0200

    resutl query object

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$db =new mysqli('127.0.0.1', 'root', '', 'appc');
+$db =new mysqli('127.0.0.1', 'root', '', 'app');
 //check is database scssesfuly connected
 if ($db->connecy_errno){
     die('Sorry we are having some problems');
--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+error_reporting(0);
 require 'connect.php';   
+
+$result =$db->query("SELECT * FROM people");
+
+print_r($result);
