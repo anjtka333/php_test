460b09695f3502073cc7681be887e2da95ccd1a6
Anna <andreikiv.ann@gmail.com>
2017-03-19 19:20:07 +0200

    updating data in db

--- index.php
+++ index.php
@@ -2,20 +2,7 @@
 error_reporting(0);
 require 'connect.php';   
 
-if ($result =$db->query("SELECT * FROM people")){
-    if($count=$result->num_rows){
-      
-      
-        while ($row = $result ->fetch_object()){
-            echo $row->first_name . '  '.$row->last_name.'</br>';
-        }
-        
-        $result->free();
-        
-        
-        
-        
-    }
-    
+if ($update=$db->query("UPDATE people SET created = NOW()")){
+    echo $db->affected_rows;
 }
 
