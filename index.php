6963a31dff20a36322d59dccf181ef9dd9922570
Anna <andreikiv.ann@gmail.com>
2017-03-19 18:41:25 +0200

    showing data from db with foreach loop

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ if ($result =$db->query("SELECT * FROM people")){
     if($count=$result->num_rows){
        echo '<p>', $count, '</p>';
         $rows = $result->fetch_all(MYSQL_ASSOC);
+        foreach ($rows as $row){
+            echo $row['first_name'] . '  '.$row['last_name'].'</br>';
+        }
         
         echo '<pre>', print_r($rows), '</pre>';
         
