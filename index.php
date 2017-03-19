74a59b963f9718a63f897b6b260443428754cafe
Anna <andreikiv.ann@gmail.com>
2017-03-19 20:16:45 +0200

    binding data

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 error_reporting(0);
 require 'connect.php';   
 
-if($insert= $db->query(
-         "INSERT INTO people(first_name, last_name, bio, created) 
-         VALUES ('Alex','Garret', 'I\'m webdew', NOW())"
-        )){
-    echo $db->affected_rows;
+if(isset ($_GET['last_name'])){
+    $last_name =trim($_GET['last_name']);
+    $people =$db->prepare("SELECT first_name, last_name FROM people WHERE last_name =?");
+    $people->bind_param('s', $last_name);
+    $people->execute();
+    
+    $people->bind_result($one, $two);
+    while ($people->fetch()){
+    echo $first_name ,' ' , $last_name;
+    }
     
 }
 
