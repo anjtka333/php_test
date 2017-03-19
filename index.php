4b390fb605c117570b897c2c0aa938feee1dfaa5
Anna <andreikiv.ann@gmail.com>
2017-03-19 19:43:09 +0200

    adding data to db

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 error_reporting(0);
 require 'connect.php';   
 
-if ($update=$db->query("DELETE FROM people WHERE id=1")){
+if($insert= $db->query(
+         "INSERT INTO people(first_name, last_name, bio, created) 
+         VALUES ('Alex','Garret', 'I\'m webdew', NOW())"
+        )){
     echo $db->affected_rows;
+    
 }
 
