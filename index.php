166e4decd6ca7b7d4ebae83515335a394852d638
Anna <andreikiv.ann@gmail.com>
2017-03-19 18:35:13 +0200

    showing bd data as assoshiative array

--- index.php
+++ index.php
@@ -3,11 +3,14 @@ error_reporting(0);
 require 'connect.php';   
 
 if ($result =$db->query("SELECT * FROM people")){
-    if($result->num_rows){
-        echo 'Yay';
+    if($count=$result->num_rows){
+       echo '<p>', $count, '</p>';
+        $rows = $result->fetch_all(MYSQL_ASSOC);
+        
+        echo '<pre>', print_r($rows), '</pre>';
+        
+        
     }
-    else {
-        die($db->error);
-    }
+    
 }
 
