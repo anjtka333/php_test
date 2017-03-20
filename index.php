68d03d786bdc53187f5687b90deeff09239427c7
Anna <andreikiv.ann@gmail.com>
2017-03-20 11:34:13 +0200

    binding multiple data

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 error_reporting(0);
 require 'connect.php';   
 
-if(isset ($_GET['last_name'])){
+if(isset ($_GET['last_name'], $_GET['id'])){
+    
     $last_name =trim($_GET['last_name']);
-    $people =$db->prepare("SELECT first_name, last_name FROM people WHERE last_name =?");
-    $people->bind_param('s', $last_name);
+    $id =trim($_GET['id']);
+    $people =$db->prepare("SELECT first_name, last_name FROM people WHERE last_name=? AND id=?");
+    $people->bind_param('si', $last_name, $id);
     $people->execute();
     
-    $people->bind_result($one, $two);
+    $people->bind_result($first_name, $last_name);
     while ($people->fetch()){
     echo $first_name ,' ' , $last_name;
     }
